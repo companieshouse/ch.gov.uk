18ac02c920764382ddeffc3074908b8658e1f2b6
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-11-08 17:10:13 +0000

    BI-11899: Get static pages with filepath rendering without errors. - But with no basket link as yet.

--- lib/ChGovUk/Controllers/StaticPages.pm
+++ lib/ChGovUk/Controllers/StaticPages.pm
@@ -3,6 +3,7 @@ package ChGovUk::Controllers::StaticPages;
 use CH::Perl;
 use Mojo::Base 'Mojolicious::Controller';
 use CH::Util::CVConstants;
+use Data::Dumper;
 
 #-------------------------------------------------------------------------------
 
@@ -50,12 +51,61 @@ sub home {
 
 sub render_filepath {
     my ($self) = @_;
+
+    debug "render_filepath: filepath = %s",  $self->param('filepath'), [HOMEPAGE];
+    my $path = 'static_pages/help/' . $self->param('filepath');
+    debug "render_filepath: \$path = %s",  $path, [HOMEPAGE];
+
+    $self->render_later;
+
+    if ($self->is_signed_in) {
+        $self->ch_api->basket->get->on(
+            success        => sub {
+                my ($api, $tx) = @_;
+
+                debug "success", [HOMEPAGE];
+
+                my $json = $tx->res->json;
+                my $show_basket_link = $json->{data}{enrolled} || undef;
+                my $items = scalar @{$json->{data}{items} || []};
+                if ($show_basket_link) {
+                    debug "User [%s] enrolled for multi-item basket; displaying basket link", $self->user_id, [HOMEPAGE];
+                }
+                else {
+                    debug "User [%s] not enrolled for multi-item basket; not displaying basket link", $self->user_id, [HOMEPAGE];
+                }
+                $self->do_render_2($items, $show_basket_link, $path);
+            },
+            # TODO BI-11899 What's wrong with this?
+            # not_authorised => sub {
+            #     my ($api, $tx) = @_;
+            #     debug "User not authenticated; not displaying basket link", [HOMEPAGE];
+            #     $self->do_render(0, undef);
+            # },
+            failure        => sub {
+                my ($api, $tx) = @_;
+                debug "Failure returned by getBasketLinks endpoint; not displaying basket link", [HOMEPAGE];
+                $self->do_render_2(0, undef, $path);
+            },
+            error          => sub {
+                my ($api, $tx) = @_;
+                debug "Error returned by getBasketLinks endpoint; not displaying basket link", [HOMEPAGE];
+                $self->do_render_2(0, undef, $path);
+            }
+        )->execute;
+    } else {
+        debug "User not signed in; not displaying basket link", [HOMEPAGE];
+        $self->do_render_2(0, undef, $path);
+    }
+
     my $path = 'static_pages/help/' . $self->param('filepath');
     $self->render(template => $path );
 }
 
 sub do_render {
     my ($self, $basket_items, $show_basket_link) = @_;
+
+    debug "do_render(%s, %s)", $basket_items, $show_basket_link [HOMEPAGE];
 
     my $search_type = 'all';
 
@@ -66,6 +116,21 @@ sub do_render {
         show_basket_link => $show_basket_link
     );
     $self->render;
+}
+
+sub do_render_2 {
+    my ($self, $basket_items, $show_basket_link, $path) = @_;
+
+    debug "do_render_2(%s, %s, %s)", $basket_items, $show_basket_link, $path [HOMEPAGE];
+
+    $self->stash(
+        basket_items     => $basket_items,
+        show_basket_link => $show_basket_link
+    );
+
+    debug "stash AFTER = %s", Dumper($self->stash), [HOMEPAGE];
+
+    $self->render(template => $path);
 }
 
 #===============================================================================
