bba811b967cb68893051d4cf1413ba3e36418bd8
caismith-ch <csmith2@companieshouse.gov.uk>
2023-01-12 10:17:35 +0000

    Add basket link to personal appointments pages

--- lib/ChGovUk/Controllers/PersonalAppointments.pm
+++ lib/ChGovUk/Controllers/PersonalAppointments.pm
@@ -41,6 +41,9 @@ sub get {
 
     trace 'Fetching appointments page [%s] for officer [%s]', $page, $officer_id;
 
+    # Get the basket link for the user nav bar
+    $self->get_basket_link;
+
     $self->ch_api->officers($officer_id)->appointments({
         filter         => $filter,
         items_per_page => $items_per_page,
@@ -104,6 +107,68 @@ sub get {
             return $self->render_exception("Error retrieving officer appointments: $error");
         },
     )->execute;
+}
+
+# ------------------------------------------------------------------------------
+
+sub get_basket_link {
+    my ( $self ) = @_;
+        if ($self->is_signed_in) {
+        $self->ch_api->basket->get->on(
+            success        => sub {
+                my ($api, $tx) = @_;
+                my $json = $tx->success->json;
+                my $show_basket_link = $json->{data}{enrolled} || undef;
+                my $items = scalar @{$json->{data}{items} || []};
+                if ($show_basket_link) {
+                    debug "User [%s] enrolled for multi-item basket; displaying basket link", $self->user_id, [PERSONAL_APPOINTMENTS];
+                }
+                else {
+                    debug "User [%s] not enrolled for multi-item basket; not displaying basket link", $self->user_id, [PERSONAL_APPOINTMENTS];
+                }
+                $self->stash_basket_link($show_basket_link, $items);
+            },
+            not_authorised => sub {
+                my ($api, $tx) = @_;
+                warn "User not authenticated; not displaying basket link", [PERSONAL_APPOINTMENTS];
+                $self->stash_basket_link(undef, 0);
+            },
+            failure        => sub {
+                my ($api, $tx) = @_;
+                log_error($tx, "failure");
+                $self->stash_basket_link(undef, 0);
+            },
+            error          => sub {
+                my ($api, $tx) = @_;
+                log_error($tx, "error");
+                $self->stash_basket_link(undef, 0);
+            }
+        )->execute;
+    } else {
+        $self->stash_basket_link(undef, 0);
+    }
+}
+
+# ------------------------------------------------------------------------------
+
+sub stash_basket_link {
+    my ( $self, $show_basket_link, $basket_items ) = @_;
+
+    $self->stash(
+        show_basket_link => $show_basket_link,
+        basket_items     => $basket_items
+    );
+}
+
+# ------------------------------------------------------------------------------
+
+sub log_error {
+    my($tx, $error_type) = @_;
+
+    my $error_code = $tx->error->{code} // 0;
+    my $error_message = $tx->error->{message} // 0;
+    my $error = (defined $error_code ? "[$error_code] " : '').$error_message;
+    error "%s returned by getBasketLinks endpoint: '%s'. Not displaying basket link.", uc $error_type, $error, [PERSONAL_APPOINTMENTS];
 }
 
 # ------------------------------------------------------------------------------
