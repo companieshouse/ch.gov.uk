4fcdf80dfbd05ff143467169ba9be7ae1ed8f476
Michael Acreman <macreman.ch@gmail.com>
2025-03-12 12:24:51 +0000

    source perlbrew in docker start script before using it

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -6,5 +6,6 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PORT=${PORT:=10000}
 
+source /opt/perlbrew/etc/bashrc
 export PERL5LIB="${APP_DIR}/local/lib/perl5"
 exec perlbrew exec perl "${APP_DIR}/script/start_app" daemon -l "http://*:${PORT}"
