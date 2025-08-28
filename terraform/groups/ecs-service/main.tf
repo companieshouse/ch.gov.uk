8a50b4b99aeb8b624989e79716326c7ab1c0d0e6
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2025-08-28 11:20:39 +0100

    Reconfigure search in Dev for multiple tasks for instance, fix task min/max, drop instance sizing for Officers

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -163,8 +163,8 @@ module "ecs-service-search" {
   desired_task_count                   = var.desired_task_count_search
   min_task_count                       = var.min_task_count_search
   max_task_count                       = var.max_task_count_search
-  required_cpus                        = local.task_required_cpu_search
-  required_memory                      = local.task_required_mem_search
+  required_cpus                        = var.required_cpus_search
+  required_memory                      = var.required_memory_search
   service_autoscale_enabled            = var.service_autoscale_enabled
   service_autoscale_target_value_cpu   = var.service_autoscale_target_value_cpu
   service_autoscale_scale_in_cooldown  = var.service_autoscale_scale_in_cooldown
